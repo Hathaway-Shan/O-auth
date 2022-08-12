7359782931f27af4933e4bae56819625532b339b
Hathaway-Shan <hathaway.shan@gmail.com>
2022-08-12 12:52:09 -0700

    test for displaying list of posts to authenticated user passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-const { agent } = require('supertest');
+const agent = request.agent(app);
 
 jest.mock('../lib/services/github');
 
@@ -33,24 +33,17 @@ describe('backend-express-template routes', () => {
 
     expect(res.status).toBe(200);
 
-    res = await agent(app).delete('/api/v1/github/');
+    res = await agent.delete('/api/v1/github/');
     expect(res.body).toEqual({
       message: 'log out successful',
     });
   });
   it('#get /posts returns a list of posts to an authenticated user', async () => {
-    let res = await request
-      .agent(app)
-      .get('/api/v1/github/callback?code=42')
-      .redirects(1);
+    await agent.get('/api/v1/github/callback?code=42').redirects(1);
 
+    const res = await agent.get('/api/v1/posts');
     expect(res.status).toBe(200);
-
-    res = await agent(app).get('/api/v1/github/posts/');
-    expect(res.status).toBe(200);
-    // expect(res.body[0]).toEqual({
-
-    // });
+    expect(res.body[0].content).toEqual('this is a post');
   });
 
   afterAll(() => {
